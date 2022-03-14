7dc0937ca4fc8dac9e949850084b0e9fd07dbaeb
Lucas_Bunny <lucas08ms@gmail.com>
2022-03-14 08:42:34 -0300

    Atualização das classes

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
@@ -2,18 +2,40 @@ package br.com.etechoracio.boa_viagem.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-
+@Table(name = "TBL_GASTO")
 public class Gasto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_GASTO")
 	private Long id;
+	
+	@Column(name = "TX_DESCRICAO")
 	private String descricao;
+	
+	@Column(name = "TX_LOCAL")
 	private String local;
+	
+	@Column(name = "TP_CATEGORIA")
 	private String categoria;
+	
+	@Column(name = "DT_GASTO")
 	private LocalDate gasto;
+	
+	@Column(name = "VLR_GASTO")
 	private Double valor;
+	
+	@Column(name = "ID_VIAGEM")
 	private Viagem viagem;
 }
